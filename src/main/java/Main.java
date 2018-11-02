a6a51131ca31b54a4dcdc5626d5e4d7b22247079
Ernestas Vaitkevicius <ownway@live.com>
2018-11-02 20:56:56 +0200

    1 versija: html setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,32 @@
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+import java.io.File;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello, World!");
+        Spark.port(80);
+        Spark.get("/home", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                URI path = Main.class.getClassLoader().getResource("home.html").toURI();
+                String html = new String(Files.readAllBytes(Paths.get(path)), Charset.forName("UTF-8"));
+                return html;
+            }
+        });
+        Spark.post("/post", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                String param = request.queryParams("vardas");
+                return "Sup, " + param;
+            }
+        });
     }
 }
